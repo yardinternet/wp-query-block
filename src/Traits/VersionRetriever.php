a5de16fe2b407e9acc991fee55f969ef62108c55
Simon <simon@yard.nl>
2024-03-26 17:27:33 +0100

    (feat): add version in composer.json

---
+++ src/Traits/VersionRetriever.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VendorName\ExamplePackage\Traits;
+
+trait VersionRetriever
+{
+	/**
+	 * Retrieves the version number from composer.json.
+	 */
+	public function getVersion(): string
+	{
+		$composerJsonPath = __DIR__ . '/../../composer.json';
+
+		if (file_exists($composerJsonPath)) {
+			$composerJson = json_decode(file_get_contents($composerJsonPath), true);
+
+			return $composerJson['version'] ?? 'unknown';
+		}
+
+		return 'unknown';
+	}
+}

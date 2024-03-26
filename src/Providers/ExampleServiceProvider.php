e844faf637ebb57d82adad2073d86f2242e2b72c
Simon <simon@yard.nl>
2024-03-26 17:28:51 +0100

    (feat): register block

--- src/Providers/ExampleServiceProvider.php
+++ src/Providers/ExampleServiceProvider.php
@@ -1,13 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace VendorName\ExamplePackage\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use VendorName\ExamplePackage\Console\ExampleCommand;
 use VendorName\ExamplePackage\Example;
+use VendorName\ExamplePackage\Traits\VersionRetriever;
 
 class ExampleServiceProvider extends ServiceProvider
 {
+    use VersionRetriever;
     /**
      * Register any application services.
      *
@@ -45,6 +49,42 @@ class ExampleServiceProvider extends ServiceProvider
             ExampleCommand::class,
         ]);
 
-        $this->app->make('Example');
+        \add_action('init', [$this, 'registerBlock']);
+        \add_filter('block_categories_all', [$this, 'addBlockCategory']);
+    }
+
+    /**
+     * Add a custom block category if it doesn't already exist.
+     */
+    public function addBlockCategory(array $categories)
+    {
+        $categories = array_merge($categories, [
+            [
+                'slug' => 'yard',
+                'title' => 'Yard',
+            ],
+        ]);
+
+        return $categories;
+    }
+
+    public function registerBlock()
+    {
+        add_action('admin_enqueue_scripts', function () {
+            wp_register_script('yard-query-block-editor-script', get_template_directory_uri().'/vendor/yard/query-block/public/index.js', get_template_directory_uri().'/vendor/yard/query-block/public/index.asset.php', $this->getVersion(), true);
+            wp_register_style('yard-query-block-style', get_template_directory_uri().'/vendor/yard/query-block/public/index.css', [], $this->getVersion(), true);
+        });
+
+        \register_block_type(__DIR__ . '/../../public/block.json', [
+            'render_callback' => [$this, 'renderBlock'],
+            'editor_script_handles' => ['yard-query-block-editor-script'],
+            'editor_style_handles' => ['yard-query-block-style'],
+        ]);
+    }
+
+
+    public function renderBlock($attributes, $content)
+    {
+        return "Hi there!";
     }
 }

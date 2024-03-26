0a72ad30c49c64142b986170d6fc933dbbe18147
Simon <simon@yard.nl>
2024-03-26 17:26:43 +0100

    (feat): add block assets

---
+++ public/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a3bd0f00e939ebff62d3');

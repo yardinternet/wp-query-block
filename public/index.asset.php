716f1c55fa1de78dc28ca62ae1a5ee433c538081
Simon <simon@yard.nl>
2024-03-28 17:19:54 +0100

    wip

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a3bd0f00e939ebff62d3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '2de2e31e5ea006310cea');

831d465de5a9e9f63c348e3d171192d169df8f05
Lara van Diemen <lara@yard.nl>
2024-04-02 15:01:01 +0200

    (feat): add manual selection select control

---
+++ assets/components/filters-controls/manual-selection-select-control.js
@@ -0,0 +1,65 @@
+/**
+ * External dependencies
+ */
+import AsyncSelect from 'react-select/async';
+import debounce from 'debounce-promise';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { mapPostsToOptions, getSubtype } from '../../utils/helpers';
+import { searchPosts } from '../../utils/api';
+
+const ManualSelectionSelectControl = ( props ) => {
+	const { attributes, setAttributes } = props;
+	const { postTypes, enableManualSelection, manualSelectionPosts } =
+		attributes;
+	const [ defaultOptions, setDefaultOptions ] = useState( [] );
+
+	/**
+	 * Load posts as options based on input value
+	 *
+	 * @param {string}   input
+	 * @param {Function} callback
+	 */
+	const loadOptions = async ( input, callback ) => {
+		if ( ! input ) return callback( [] );
+
+		const subtype = getSubtype( postTypes );
+		const posts = await searchPosts( input, subtype );
+
+		if ( ! posts ) return callback( [] );
+
+		const options = mapPostsToOptions( posts );
+		setDefaultOptions( options );
+		callback( options );
+	};
+
+	return (
+		enableManualSelection && (
+			<>
+				<p className="yard-query-inspector-label">
+					{ __( 'Vul je zoekterm in. Zoekt op hele woorden.' ) }
+				</p>
+				<AsyncSelect
+					isMulti
+					backspaceRemovesValue={ false }
+					defaultOptions={ defaultOptions }
+					value={ manualSelectionPosts }
+					onChange={ ( selectedPosts ) =>
+						setAttributes( { manualSelectionPosts: selectedPosts } )
+					}
+					loadOptions={ debounce( loadOptions, 500 ) }
+				/>
+			</>
+		)
+	);
+};
+
+export default ManualSelectionSelectControl;

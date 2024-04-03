f6f7b52b74e0dc57bb9c1e5343a45d0198e1e1bd
Lara van Diemen <lara@yard.nl>
2024-04-03 12:24:29 +0200

    (feat): add sortable select to manual selection select control

--- assets/components/filters-controls/manual-selection-select-control.js
+++ assets/components/filters-controls/manual-selection-select-control.js
@@ -1,14 +1,25 @@
 /**
  * External dependencies
  */
+import { components } from 'react-select';
 import AsyncSelect from 'react-select/async';
 import debounce from 'debounce-promise';
+
+import { closestCorners, DndContext } from '@dnd-kit/core';
+import { restrictToParentElement } from '@dnd-kit/modifiers';
+import {
+	arrayMove,
+	horizontalListSortingStrategy,
+	SortableContext,
+	useSortable,
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useEffect, useState } from '@wordpress/element';
+import { useEffect, useState, useCallback } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -16,10 +27,52 @@ import { useEffect, useState } from '@wordpress/element';
 import { mapPostsToOptions, getSubtype } from '../../utils/helpers';
 import { searchPosts } from '../../utils/api';
 
+const MultiValue = ( props ) => {
+	const onMouseDown = ( e ) => {
+		e.preventDefault();
+		e.stopPropagation();
+	};
+	const innerProps = { ...props.innerProps, onMouseDown };
+	const { attributes, listeners, setNodeRef, transform } = useSortable( {
+		id: props.data.value,
+	} );
+	const style = {
+		transform: CSS.Transform.toString( transform ),
+	};
+
+	return (
+		<div
+			className="yard-sortable-select__multi-value__wrapper"
+			style={ style }
+			ref={ setNodeRef }
+			{ ...attributes }
+			{ ...listeners }
+		>
+			<components.MultiValue { ...props } innerProps={ innerProps } />
+		</div>
+	);
+};
+
+const MultiValueRemove = ( props ) => {
+	return (
+		<components.MultiValueRemove
+			{ ...props }
+			innerProps={ {
+				onPointerDown: ( e ) => e.stopPropagation(),
+				...props.innerProps,
+			} }
+		/>
+	);
+};
+
 const ManualSelectionSelectControl = ( props ) => {
 	const { attributes, setAttributes } = props;
-	const { postTypes, enableManualSelection, manualSelectionPosts } =
-		attributes;
+	const {
+		postTypes,
+		postsPerPage,
+		enableManualSelection,
+		manualSelectionPosts,
+	} = attributes;
 	const [ defaultOptions, setDefaultOptions ] = useState( [] );
 
 	/**
@@ -58,22 +111,77 @@ const ManualSelectionSelectControl = ( props ) => {
 		callback( options );
 	};
 
+	const handleDragEnd = useCallback(
+		( event ) => {
+			const { active, over } = event;
+
+			if ( ! active || ! over ) return;
+
+			const oldIndex = manualSelectionPosts.findIndex(
+				( item ) => item.value === active.id
+			);
+			const newIndex = manualSelectionPosts.findIndex(
+				( item ) => item.value === over.id
+			);
+			const newArray = arrayMove(
+				manualSelectionPosts,
+				oldIndex,
+				newIndex
+			);
+
+			setAttributes( { manualSelectionPosts: newArray } );
+		},
+		[ manualSelectionPosts, setAttributes ]
+	);
+
 	return (
 		enableManualSelection && (
 			<>
 				<p className="yard-query-inspector-label">
 					{ __( 'Vul je zoekterm in. Zoekt op hele woorden.' ) }
 				</p>
-				<AsyncSelect
-					isMulti
-					backspaceRemovesValue={ false }
-					defaultOptions={ defaultOptions }
-					value={ manualSelectionPosts }
-					onChange={ ( selectedPosts ) =>
-						setAttributes( { manualSelectionPosts: selectedPosts } )
-					}
-					loadOptions={ debounce( loadOptions, 500 ) }
-				/>
+				<DndContext
+					modifiers={ [ restrictToParentElement ] }
+					onDragEnd={ handleDragEnd }
+					collisionDetection={ closestCorners }
+				>
+					<SortableContext
+						items={ defaultOptions.map(
+							( option ) => option.value
+						) }
+						strategy={ horizontalListSortingStrategy }
+					>
+						<AsyncSelect
+							backspaceRemovesValue={ false }
+							className="yard-sortable-select"
+							classNamePrefix="yard-sortable-select"
+							closeMenuOnSelect={ false }
+							components={ {
+								MultiValue,
+								MultiValueRemove,
+							} }
+							defaultOptions={ defaultOptions }
+							isMulti
+							isOptionDisabled={ () =>
+								manualSelectionPosts.length >= postsPerPage
+							}
+							loadingMessage={ () => __( 'Laden…' ) }
+							loadOptions={ debounce( loadOptions, 500 ) }
+							noOptionsMessage={ () =>
+								__(
+									'Geen berichten gevonden. Probeer een andere zoekterm.'
+								)
+							}
+							onChange={ ( selectedPosts ) =>
+								setAttributes( {
+									manualSelectionPosts: selectedPosts,
+								} )
+							}
+							placeholder={ __( 'Selecteer berichten…' ) }
+							value={ manualSelectionPosts }
+						/>
+					</SortableContext>
+				</DndContext>
 			</>
 		)
 	);

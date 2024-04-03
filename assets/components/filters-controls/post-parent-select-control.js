16ad20e4206a607136d4921abbf9e1003b4d7bb4
Lara van Diemen <lara@yard.nl>
2024-04-03 14:42:48 +0200

    (refactor): create shared components for async select posts

--- assets/components/filters-controls/post-parent-select-control.js
+++ assets/components/filters-controls/post-parent-select-control.js
@@ -1,96 +1,40 @@
 /**
- * External dependencies
- */
-import AsyncSelect from 'react-select/async';
-import debounce from 'debounce-promise';
-
-/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { mapPostsToOptions, getSubtype } from '../../utils/helpers';
-import { searchPosts } from '../../utils/api';
+import AsyncSelectPostsControl from '../shared/async-select-posts-control';
 
 const PostParentSelectControl = ( props ) => {
 	const { attributes, setAttributes } = props;
 	const {
-		postTypes,
 		postParentOption,
 		postParent,
 		enableManualSelection,
 		enablePostParent,
 	} = attributes;
-	const [ defaultOptions, setDefaultOptions ] = useState( [] );
-
-	/**
-	 * Load posts on init
-	 */
-	useEffect( () => {
-		const getOptions = async () => {
-			const subtype = getSubtype( postTypes );
-			const posts = await searchPosts( '', subtype );
-
-			if ( ! posts ) return;
-
-			const options = mapPostsToOptions( posts );
-			setDefaultOptions( options );
-		};
-
-		getOptions();
-	}, [ postTypes ] );
-
-	/**
-	 * Load posts as options based on input value
-	 *
-	 * @param {string}   input
-	 * @param {Function} callback
-	 */
-	const loadOptions = async ( input, callback ) => {
-		if ( ! input ) return callback( [] );
-
-		const subtype = getSubtype( postTypes );
-		const posts = await searchPosts( input, subtype );
-
-		if ( ! posts ) return callback( [] );
-
-		const options = mapPostsToOptions( posts );
-		setDefaultOptions( options );
-		callback( options );
-	};
 
 	return (
-		! enableManualSelection &&
-		enablePostParent &&
-		postParentOption === 'specific-parent' && (
-			<>
-				<p className="yard-query-inspector-label">
-					{ __(
-						'Selecteer het hoofdbericht waar de subberichten van getoond moeten worden.'
-					) }
-				</p>
-				<AsyncSelect
-					defaultOptions={ defaultOptions }
-					isClearable={ true }
-					loadingMessage={ () => __( 'Laden…' ) }
-					loadOptions={ debounce( loadOptions, 500 ) }
-					noOptionsMessage={ () =>
-						__(
-							'Geen berichten gevonden. Probeer een andere zoekterm.'
-						)
-					}
-					onChange={ ( selectedPost ) =>
-						setAttributes( { postParent: selectedPost } )
-					}
-					placeholder={ __( 'Selecteer bericht…' ) }
-					value={ postParent }
-				/>
-			</>
-		)
+		<AsyncSelectPostsControl
+			attributes={ attributes }
+			enable={
+				! enableManualSelection &&
+				enablePostParent &&
+				postParentOption === 'specific-parent'
+			}
+			handleChange={ ( selectedPost ) =>
+				setAttributes( { postParent: selectedPost } )
+			}
+			isClearable={ true }
+			isMulti={ false }
+			label={ __(
+				'Selecteer het hoofdbericht waar de subberichten van getoond moeten worden.'
+			) }
+			value={ postParent }
+		/>
 	);
 };
 
---
+++ assets/components/shared/async-select-posts-control.js
@@ -0,0 +1,106 @@
+/**
+ * External dependencies
+ */
+import AsyncSelect from 'react-select/async';
+import debounce from 'debounce-promise';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useState, useEffect, useCallback } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { mapPostsToOptions, getSubtype } from '../../utils/helpers';
+import { searchPosts } from '../../utils/api';
+
+const AsyncSelectPostsControl = ( props ) => {
+	const {
+		attributes,
+		enable,
+		handleChange,
+		isClearable,
+		isMulti,
+		label,
+		value,
+	} = props;
+	const { postTypes } = attributes;
+	const [ defaultOptions, setDefaultOptions ] = useState( [] );
+
+	/**
+	 * Search posts based on input value
+	 *
+	 * @param {string} input
+	 */
+	const getPostsAsOptions = useCallback(
+		async ( input ) => {
+			if ( ! input ) input = '';
+
+			const subtype = getSubtype( postTypes );
+			const posts = await searchPosts( input, subtype );
+
+			return posts ? mapPostsToOptions( posts ) : null;
+		},
+		[ postTypes ]
+	);
+
+	/**
+	 * Load posts on init
+	 */
+	useEffect( () => {
+		const getOptions = async () => {
+			const options = await getPostsAsOptions();
+			if ( options ) setDefaultOptions( options );
+		};
+
+		getOptions();
+	}, [ getPostsAsOptions ] );
+
+	/**
+	 * Load posts based on input value when the user start searching
+	 *
+	 * @param {string}   input
+	 * @param {Function} callback
+	 */
+	const loadOptions = async ( input, callback ) => {
+		if ( ! input ) return callback( [] );
+
+		const options = await getPostsAsOptions( input );
+
+		if ( options ) {
+			setDefaultOptions( options );
+			callback( options );
+		} else {
+			callback( [] );
+		}
+	};
+
+	return (
+		enable && (
+			<>
+				<p className="yard-query-inspector-label">{ label }</p>
+				<AsyncSelect
+					backspaceRemovesValue={ false }
+					closeMenuOnSelect={ ! isMulti }
+					defaultOptions={ defaultOptions }
+					isClearable={ isClearable }
+					isMulti={ isMulti }
+					loadingMessage={ () => __( 'Laden…' ) }
+					loadOptions={ debounce( loadOptions, 500 ) }
+					noOptionsMessage={ () =>
+						__(
+							'Geen berichten gevonden. Probeer een andere zoekterm.'
+						)
+					}
+					onChange={ handleChange }
+					placeholder={ __( 'Selecteer bericht…' ) }
+					value={ value }
+				/>
+			</>
+		)
+	);
+};
+
+export default AsyncSelectPostsControl;

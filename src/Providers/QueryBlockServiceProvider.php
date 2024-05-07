4a959e768ac1bf00f95ce1cbee9365967d2f9825
Simon <simon@yard.nl>
2024-05-07 12:00:36 +0200

    (feat): cleanup files

--- src/Providers/QueryBlockServiceProvider.php
+++ src/Providers/QueryBlockServiceProvider.php
@@ -13,83 +13,72 @@ use Yard\QueryBlock\Traits\VersionRetriever;
 
 class QueryBlockServiceProvider extends ServiceProvider
 {
-	use VersionRetriever;
-	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->singleton('Example', function () {
-			return new Example($this->app);
-		});
+    use VersionRetriever;
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-		$this->mergeConfigFrom(
-			__DIR__.'/../../config/example.php',
-			'example'
-		);
-	}
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(
+            __DIR__.'/../../resources/views',
+            'QueryBlock',
+        );
 
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->publishes([
-			__DIR__.'/../../config/example.php' => $this->app->configPath('example.php'),
-		], 'config');
+        $this->publishes([
+            __DIR__.'/../../resources/views' => resource_path('views/vendor/yard-query-block'),
+        ]);
 
-		$this->loadViewsFrom(
-			__DIR__.'/../../resources/views',
-			'Query',
-		);
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
-		$this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        \add_action('init', [$this, 'registerBlock']);
+        \add_filter('block_categories_all', [$this, 'addBlockCategory']);
+    }
 
-		$this->commands([
-			ExampleCommand::class,
-		]);
+    /**
+     * Add a custom block category if it doesn't already exist.
+     */
+    public function addBlockCategory(array $categories)
+    {
+        $categories = array_merge($categories, [
+            [
+                'slug' => 'yard',
+                'title' => 'Yard',
+            ],
+        ]);
 
-		\add_action('init', [$this, 'registerBlock']);
-		\add_filter('block_categories_all', [$this, 'addBlockCategory']);
-	}
+        return $categories;
+    }
 
-	/**
-	 * Add a custom block category if it doesn't already exist.
-	 */
-	public function addBlockCategory(array $categories)
-	{
-		$categories = array_merge($categories, [
-			[
-				'slug' => 'yard',
-				'title' => 'Yard',
-			],
-		]);
+    public function registerBlock()
+    {
+        add_action('admin_enqueue_scripts', function () {
+            wp_register_script('yard-query-block-editor-script', $this->route('/yard/query-block/assets/js/index'), $this->route('/yard/query-block/assets/php/index'), $this->getVersion(), true);
+            wp_register_style('yard-query-block-style', $this->route('/yard/query-block/assets/css/index'), [], $this->getVersion());
+        });
 
-		return $categories;
-	}
+        \register_block_type(__DIR__ . '/../../public/block.json', [
+            'render_callback' => [$this, 'renderBlock'],
+            'editor_script_handles' => ['yard-query-block-editor-script'],
+            'editor_style_handles' => ['yard-query-block-style'],
+        ]);
+    }
 
-	public function registerBlock()
-	{
-		add_action('admin_enqueue_scripts', function () {
-			wp_register_script('yard-query-block-editor-script', $this->route('/yard/query-block/assets/js/index'), $this->route('/yard/query-block/assets/php/index'), $this->getVersion(), true);
-			wp_register_style('yard-query-block-style', $this->route('/yard/query-block/assets/css/index'), [], $this->getVersion());
-		});
-
-		\register_block_type(__DIR__ . '/../../public/block.json', [
-			'render_callback' => [$this, 'renderBlock'],
-			'editor_script_handles' => ['yard-query-block-editor-script'],
-			'editor_style_handles' => ['yard-query-block-style'],
-		]);
-	}
-
-	public function route($path)
-	{
-		return config('app.url') . $path;
-	}
+    public function route($path)
+    {
+        return config('app.url') . $path;
+    }
 
 	public function renderBlock($attributes, $content)
 	{

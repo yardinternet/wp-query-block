d34bdb695ad7316059fb445b1dfcdca74317f23f
Simon <simon@yard.nl>
2024-05-07 16:58:16 +0200

    (feat): create templates route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,13 +5,38 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::get('/yard/query-block/assets/js/index', function () {
-	return response()->file(__DIR__.'/../../public/index.js', ['Content-Type' => 'application/javascript']);
+    return response()->file(__DIR__.'/../../public/index.js', ['Content-Type' => 'application/javascript']);
 });
 
 Route::get('yard/query-block/assets/php/index', function () {
-	return response()->file(__DIR__.'/../../public/index.asset.php', ['Content-Type' => 'application/x-httpd-php']);
+    return response()->file(__DIR__.'/../../public/index.asset.php', ['Content-Type' => 'application/x-httpd-php']);
 });
 
 Route::get('yard/query-block/assets/css/index', function () {
-	return response()->file(__DIR__.'/../../public/index.css', ['Content-Type' => 'text/css']);
+    return response()->file(__DIR__.'/../../public/index.css', ['Content-Type' => 'text/css']);
+});
+
+Route::get('yard/query-block/settings/templates', function () {
+    $path = resource_path('views/vendor/yard-query-block/templates');
+    $files = scandir($path);
+    $templates = [];
+
+    foreach ($files as $file) {
+        if (strpos($file, '.blade.php') === false) {
+            continue;
+        }
+
+        $content = file_get_contents($path . '/' . $file);
+        preg_match('/Template: (.*)\n/', $content, $matches);
+        $templateName = $matches[1] ?? $file;
+
+        $templates[] = [
+            'label' => $templateName,
+            'value' => str_replace('.blade.php', '', $file),
+        ];
+    }
+
+    return response()->json([
+        'templates' => $templates,
+    ]);
 });

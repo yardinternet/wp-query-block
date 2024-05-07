810448c15195128764584c5d77d65c4518654382
Simon <simon@yard.nl>
2024-05-07 15:20:12 +0200

    (chore): update default template

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -5,33 +5,23 @@
 	 */
 @endphp
 
-<div>
-	@if ($postDataCollection->isNotEmpty())
-		<div class="row">
-			@foreach ($postDataCollection as $postData)
-				@switch($postData->postType())
-					@case('foo')
-						{{-- bar --}}
-					@break
 
-					@default
-						<div class="col-md-6 col-lg-4 mb-lg-4 mb-3">
-							@include('partials.card.card-default', [
-								'cardClass' => 'has-gradient-border',
-								'displayFeaturedImage' => $attributes->displayImage(),
-								'displayDate' => $attributes->displayDate(),
-								'displayExcerpt' => $attributes->displayExcerpt(),
-								'thumbnailUrl' => $postData->thumbnail()?->url(),
-								'dateTime' => $postData->date(),
-								'dateString' => $postData->date('j F Y'),
-								'title' => $postData->title(),
-								'link' => $postData->url(),
-								'excerpt' => $postData->excerpt(),
-							])
-						</div>
-					@break
-				@endswitch
-			@endforeach
-		</div>
-	@endif
-</div>
+@forelse ($postDataCollection as $postData)
+	<div>
+		<h3>{{ $postData->title() }}</h3>
+
+		@if ($attributes->displayDate())
+			<time datetime="{{ $postData->date }}">{{ $postData->date() }}</time>
+		@endif
+
+		@if ($attributes->displayImage() && $postData->hasThumbnail())
+			<img src="{{ $postData->thumbnail()?->url('thumbnail') }}" alt="{{ $postData->thumbnail()?->alt() }}">
+		@endif
+
+		@if ($attributes->displayExcerpt() && $postData->excerpt())
+			<p>{!! $postData->excerpt() !!}</p>
+		@endif
+	</div>
+@empty
+	<p>Geen resultaten gevonden.</p>
+@endforelse

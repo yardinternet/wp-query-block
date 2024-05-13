f8b6b4335b84013ce19cf7a778b75ad97d4a79bf
Lara van Diemen <lara@yard.nl>
2024-05-13 14:11:27 +0200

    (feat): fetch template options for select control

--- assets/components/display-controls/template-select-control.js
+++ assets/components/display-controls/template-select-control.js
@@ -2,20 +2,39 @@
  * WordPress dependencies
  */
 import { SelectControl } from '@wordpress/components';
+import { useEffect, useState, useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { fetchBlockSettings } from '../../utils/api';
+
+const DEFAULT_TEMPLATE_OPTIONS = [ { label: 'Standaard', value: 'default' } ];
 
 const TemplateSelectControl = ( props ) => {
 	const { setAttributes, attributes } = props;
 	const { template } = attributes;
 
-	const templateOptions = [ { label: 'Standaard', value: 'default' } ];
+	const [ templateOptions, setTemplateOptions ] = useState(
+		DEFAULT_TEMPLATE_OPTIONS
+	);
 
-	/**
-	 * @todo Retrieve from template if option is available and what the options are
-	 *
-	 * Options:
-	 * - Template name (not the file name)
-	 */
+	const getTemplateOptions = useCallback( async () => {
+		try {
+			const settings = await fetchBlockSettings();
+
+			if ( settings?.templates.length > 1 ) {
+				setTemplateOptions( settings.templates );
+			}
+		} catch ( error ) {
+			setTemplateOptions( DEFAULT_TEMPLATE_OPTIONS );
+		}
+	}, [] );
+
+	useEffect( () => {
+		getTemplateOptions();
+	}, [ getTemplateOptions ] );
 
 	return (
 		<SelectControl

82af408c16f6d556d1e0d2e22d96936933004b01
Simon <simon@yard.nl>
2024-05-14 12:03:42 +0200

    (feat): new settings route url

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,28 +15,3 @@ Route::get('yard/query-block/assets/php/index', function () {
 Route::get('yard/query-block/assets/css/index', function () {
     return response()->file(__DIR__.'/../../public/index.css', ['Content-Type' => 'text/css']);
 });
-
-Route::get('yard/query-block/settings/templates', function () {
-    $path = resource_path('views/vendor/yard-query-block/templates');
-    $files = scandir($path);
-    $templates = [];
-
-    foreach ($files as $file) {
-        if (strpos($file, '.blade.php') === false) {
-            continue;
-        }
-
-        $content = file_get_contents($path . '/' . $file);
-        preg_match('/Template: (.*)\n/', $content, $matches);
-        $templateName = $matches[1] ?? $file;
-
-        $templates[] = [
-            'label' => $templateName,
-            'value' => str_replace('.blade.php', '', $file),
-        ];
-    }
-
-    return response()->json([
-        'templates' => $templates,
-    ]);
-});

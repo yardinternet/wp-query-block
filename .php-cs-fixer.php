7809a0591ac5f57026ee81012d8b6632580e609f
Simon <simon@yard.nl>
2024-05-21 12:16:56 +0200

    (fix): php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,28 +5,15 @@ declare(strict_types=1);
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
-# PHP CS Fixer can be run using the VSCode extension on save
-# or by using the command line `composer run-scripts format`
-# Our current setup only formats PHP files, not blade.php files.
-# However, you can install Laravel Blade Formatter (shufo.vscode-blade-formatter)
-# which, if installed globally, also supports the command line `blade-formatter --write **/*.blade.php.`
+# PHP CS Fixer can be run by using the composer script `composer format`
 
 $finder = Finder::create()
     ->in(__DIR__)
-    ->in(__DIR__.'/web/app/themes')
     ->name('*.php')
-    ->name('_ide_helper')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true)
-
-    ->exclude('app/cache')
-    ->exclude('dist')
-    ->exclude('mu-plugins')
-    ->exclude('node_modules')
-    ->exclude('plugins')
-    ->exclude('vendor')
-    ->exclude('web/wp')
+    ->exclude('public')
 ;
 
 return (new Config)

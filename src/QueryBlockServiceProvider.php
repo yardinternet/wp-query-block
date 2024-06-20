848b9c90597c4cbf842a095b38968d0b11dd4c52
SimonvanWijhe <SimonvanWijhe@users.noreply.github.com>
2024-06-20 12:28:33 +0000

    (auto): apply php-cs-fixer changes

--- src/QueryBlockServiceProvider.php
+++ src/QueryBlockServiceProvider.php
@@ -4,27 +4,27 @@ declare(strict_types=1);
 
 namespace Yard\QueryBlock;
 
-use Yard\QueryBlock\Block\Block;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Yard\QueryBlock\Block\Block;
 
 class QueryBlockServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
-	{
-		$package
-			->name('yard-query-block')
-			->hasViews()
-			->hasRoute('web');
-	}
+    {
+        $package
+            ->name('yard-query-block')
+            ->hasViews()
+            ->hasRoute('web');
+    }
 
-	public function packageRegistered(): void
+    public function packageRegistered(): void
     {
         $this->app->singleton('Block', fn () => new Block($this->app));
     }
 
     public function packageBooted(): void
     {
-		$this->app->make('Block')->register();
+        $this->app->make('Block')->register();
     }
 }

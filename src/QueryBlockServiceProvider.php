8d03157a2ce835faf02377ade7edd323c6df312e
Simon <simon@yard.nl>
2024-06-20 12:05:07 +0200

    (refactor): move block registration out of serviceprovider

--- src/QueryBlockServiceProvider.php
+++ src/QueryBlockServiceProvider.php
@@ -4,18 +4,11 @@ declare(strict_types=1);
 
 namespace Yard\QueryBlock;
 
-use Illuminate\Contracts\View\View;
+use Yard\QueryBlock\Block\Block;
 use Illuminate\Support\ServiceProvider;
-use WP_REST_Response;
-use Yard\Data\PostData;
-use Yard\QueryBlock\Block\BlockAttributes;
-use Yard\QueryBlock\Query\PostQuery;
-use Yard\QueryBlock\Traits\VersionRetriever;
 
 class QueryBlockServiceProvider extends ServiceProvider
 {
-    use VersionRetriever;
-
     public function register(): void
     {
         //
@@ -34,113 +27,6 @@ class QueryBlockServiceProvider extends ServiceProvider
 
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
-        \add_action('init', [$this, 'registerBlock']);
-        \add_filter('block_categories_all', [$this, 'addBlockCategory']);
-        \add_action('rest_api_init', function () {
-            \register_rest_route('yard/query-block/v1', '/settings', [
-                'methods' => 'GET',
-                'callback' => $this->blockSettings(...),
-                'permission_callback' => '__return_true',
-            ]);
-        });
-    }
-
-    /**
-     * Add a custom block category if it doesn't already exist.
-     *
-     * @param list<array{slug: string, title: string}> $categories
-     *
-     * @return list<array{slug: string, title: string}>
-     */
-    public function addBlockCategory(array $categories): array
-    {
-        if (in_array('yard', array_column($categories, 'slug'))) {
-            return $categories;
-        }
-
-        $categories = array_merge($categories, [
-            [
-                'slug' => 'yard',
-                'title' => 'Yard',
-            ],
-        ]);
-
-        return $categories;
-    }
-
-    public function registerBlock(): void
-    {
-        add_action('admin_enqueue_scripts', function () {
-            $deps = require __DIR__.'/../public/index.asset.php';
-            wp_register_script('yard-query-block-editor-script', $this->route('/yard/query-block/assets/js/index'), $deps['dependencies'], $this->getVersion(), true);
-            wp_register_style('yard-query-block-style', $this->route('/yard/query-block/assets/css/index'), [], $this->getVersion());
-        });
-
-        \register_block_type(__DIR__ . '/../public/block.json', [
-            'render_callback' => [$this, 'renderBlock'],
-            'editor_script_handles' => ['yard-query-block-editor-script'],
-            'editor_style_handles' => ['yard-query-block-style'],
-        ]);
-    }
-
-    public function route(string $path): string
-    {
-        return config('app.url') . $path;
-    }
-
-    public function blockSettings(): WP_REST_Response
-    {
-        $path = resource_path('views/vendor/yard-query-block/templates');
-        $files = scandir($path);
-        $templates = [];
-
-        if (false === $files) {
-            return new WP_REST_Response([
-                'templates' => [],
-            ]);
-        }
-
-        foreach ($files as $file) {
-            if (strpos($file, '.blade.php') === false) {
-                continue;
-            }
-
-            $content = file_get_contents($path . '/' . $file);
-
-            if (false === $content) {
-                continue;
-            }
-
-            preg_match('/Template: (.*)\n/', $content, $matches);
-            $templateName = $matches[1] ?? $file;
-
-            $templates[] = [
-                'label' => $templateName,
-                'value' => str_replace('.blade.php', '', $file),
-            ];
-        }
-
-        return new WP_REST_Response([
-            'templates' => $templates,
-        ]);
-    }
-
-    /**
-     * @param array{} $attributes
-     */
-    public function renderBlock(array $attributes): View
-    {
-        $attributes = BlockAttributes::from($attributes);
-
-        $results = (new PostQuery($attributes))->get();
-
-        $postDataCollection = PostData::collect($results);
-
-        $template = $attributes->template();
-
-        return view("yard-query-block::templates." . $template, [
-            'postDataCollection' => $postDataCollection,
-            'attributes' => $attributes,
-        ]);
+		(new Block())->register();
     }
 }

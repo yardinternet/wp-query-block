ef2d8303fdaa07fd3515c8ded7c927d8cf8aa004
Simon <simon@yard.nl>
2024-08-08 17:40:10 +0200

    (feat): catch exceptions

--- src/Block/Block.php
+++ src/Block/Block.php
@@ -123,14 +123,26 @@ class Block
      */
     public function renderBlock(array $attributes): View
     {
-        $attributes = BlockAttributes::from($attributes);
-        $results = (new PostQuery($attributes))->get();
-        $postDataCollection = PostData::collect($results);
-        $view = "yard-query-block::templates." . $attributes->template();
-
-        return view(view()->exists($view) ? $view : "yard-query-block::templates.default", [
-            'postDataCollection' => $postDataCollection,
-            'attributes' => $attributes,
-        ]);
+		try {
+			return $this->render($attributes);
+		} catch (\Exception $e) {
+			return view("yard-query-block::error");
+		}
     }
+
+	/**
+	 * @param array{} $attributes
+	 */
+	private function render(array $attributes): View
+	{
+		$attributes = BlockAttributes::from($attributes);
+		$results = (new PostQuery($attributes))->get();
+		$postDataCollection = PostData::collect($results);
+		$view = "yard-query-block::templates." . $attributes->template();
+
+		return view(view()->exists($view) ? $view : "yard-query-block::templates.default", [
+			'postDataCollection' => $postDataCollection,
+			'attributes' => $attributes,
+		]);
+	}
 }

3e03abc5cc6943dca7611458f391c217ed0b34f1
Lara van Diemen <lara@yard.nl>
2025-04-23 11:14:48 +0200

    (chore): add yard-popup to exclude post types

--- public/index.asset.php
+++ public/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '519d7cca724968ab22f5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'cf6ebf6d4a4edc4ffcc7');

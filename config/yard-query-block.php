565ed842c561996df8c41201c04c0571f7d3f4bd
Yard workflows <helpdesk@yard.nl>
2026-03-10 16:30:16 +0000

    style: apply php-cs-fixer changes

--- config/yard-query-block.php
+++ config/yard-query-block.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 return [
 	'connections' => [
-//		[
-//			'from' => 'news',
-//			'to' => 'project',
-//			'meta_key' => 'news_related_project',
-//		],
+		//		[
+		//			'from' => 'news',
+		//			'to' => 'project',
+		//			'meta_key' => 'news_related_project',
+		//		],
 	],
 ];

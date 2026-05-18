97503cddba2ec3a11171b1c96dd6d30f86f647d9
rivanuff <rivanuff@gmail.com>
2026-05-18 15:05:58 +0200

    feat: make taxonomyselectcontrol use asyncselect

--- assets/components/filters-controls/taxonomy-select-control.js
+++ assets/components/filters-controls/taxonomy-select-control.js
@@ -1,12 +1,14 @@
 /**
  * External dependencies
  */
-import Select from 'react-select';
+import AsyncSelect from 'react-select/async';
+import debounce from 'debounce-promise';
 
 /**
  * WordPress dependencies
  */
-import { useEffect, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { useCallback, useEffect, useMemo, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -17,18 +19,32 @@ import { mapTermsToOptions } from '../../utils/helpers';
 const TaxonomySelectControl = ( props ) => {
 	const { taxonomy, attributes, setAttributes } = props;
 	const { taxonomyTerms } = attributes;
-	const [ options, setOptions ] = useState( [] );
+	const [ defaultOptions, setDefaultOptions ] = useState( [] );
+
+	const getTermsAsOptions = useCallback(
+		async ( input = '' ) => {
+			const terms = await fetchTermsByTaxonomy(
+				taxonomy.rest_base,
+				input
+			);
+			return terms ? mapTermsToOptions( terms ) : [];
+		},
+		[ taxonomy.rest_base ]
+	);
 
 	useEffect( () => {
-		const getTerms = async () => {
-			const allTerms = await fetchTermsByTaxonomy( taxonomy.rest_base );
-			const mappedTerms = mapTermsToOptions( allTerms );
-
-			setOptions( mappedTerms );
+		const getOptions = async () => {
+			const options = await getTermsAsOptions();
+			setDefaultOptions( options );
 		};
 
-		getTerms();
-	}, [ taxonomy.rest_base ] );
+		getOptions();
+	}, [ getTermsAsOptions ] );
+
+	const loadOptions = useMemo(
+		() => debounce( getTermsAsOptions, 500 ),
+		[ getTermsAsOptions ]
+	);
 
 	/**
 	 * Save the selected terms as an attribute
@@ -49,15 +65,25 @@ const TaxonomySelectControl = ( props ) => {
 	return (
 		<>
 			<p className="yard-query-inspector-label">{ taxonomy.name }</p>
-			<Select
+			<AsyncSelect
+				backspaceRemovesValue={ false }
+				closeMenuOnSelect={ false }
+				defaultOptions={ defaultOptions }
 				isMulti
+				loadingMessage={ () =>
+					__( 'Aan het zoeken…', 'yard-query-block' )
+				}
+				loadOptions={ loadOptions }
+				noOptionsMessage={ () =>
+					__( 'Geen items gevonden.', 'yard-query-block' )
+				}
+				onChange={ onChange }
+				placeholder={ __( 'Selecteer een item', 'yard-query-block' ) }
 				value={
 					taxonomyTerms?.[ taxonomy.slug ]
 						? taxonomyTerms[ taxonomy.slug ]
 						: []
 				}
-				options={ options }
-				onChange={ onChange }
 			/>
 		</>
 	);
